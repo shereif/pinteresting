37bc233ae55340d6af2d4a97756507a7c11cc0d2
Shereif Saleh <shereifsaleh90@gmail.com>
2014-09-18 21:29:53 -0400

    Implemented Divise for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.configure do
+sRails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    #Set to host
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
